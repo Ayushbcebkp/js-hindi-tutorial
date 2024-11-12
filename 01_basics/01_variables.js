448ee65dad8fc9a27ac08ab831e4c60426108004
Ayushbcebkp <124160042+Ayushbcebkp@users.noreply.github.com>
2024-11-12 14:40:42 +0000

    some changes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,29 @@
+const accountId = 144553
+let accountEmail = "ayush@google.com"
+var accountPassword = "12345"
+accountCity ="begusarai"
+let accountState;
+
+// accountId = 2 // not allowed
+accountEmail = "ayush#45@.com"
+accountPassword = "21212"
+accountCity = "bengaluru"
+
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
+
+
+/*
+prefer not to use the var because of issue in block scope and functional scope
+
+*/
+
+
+
+
+
+
+
+
+
+
---
+++ 01_basics/02_datatype.js
@@ -0,0 +1,23 @@
+ "use strict";  //treat all js code as newer version
+ 
+// alert(3+3) // we are using node.js,not browser
+
+console.log(3+3)
+
+console.log("ayush")
+
+let name ="AYUSH"
+let age =22
+let isLoggedIn =false
+
+// number => 2 power 53
+// bigint
+// string => ""
+// boolean =>true/false
+// null           (stand alone value hai )
+// undefined =>value not assiged
+// symbol =>unique
+
+// object
+
+console.log(typeof "ayush")
