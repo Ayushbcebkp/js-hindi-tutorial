8408f14e7d68caa6208e140a33889430668c8977
Ayushbcebkp <124160042+Ayushbcebkp@users.noreply.github.com>
2024-11-13 11:47:43 +0000

    conversionoperation

---
+++ 01_basics/03_conversionoperation.js
@@ -0,0 +1,24 @@
+let score ="33abc"
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber =Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber)
+
+// 33 => 33
+// 33abc => NaN
+// true=> 1; false=>0
+
+let isLoggedIn = 1
+let BooleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(BooleanIsLoggedIn)
+
+
+// 1=>true
+// 0 =>false
+// "" => false
+// "ayush " => true
+
+// similarly we can convert the string also
